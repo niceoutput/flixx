ac6eb52a3902c2a806ab9b7c84355d51113a5fac
mario <mario@uniplaces.com>
2023-12-04 23:19:58 +0000

    add git ignore

--- js/script.js
+++ js/script.js
@@ -4,6 +4,24 @@ const global = {
 
 console.log(global.currentPage)
 
+async function displayPopularMovies() {
+  const results = await fetchAPIData('movie/popular');
+  console.log(results);
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = '5b1f7a4fa8b3886298391bea093cdf5a';
+  const API_URL = 'https://api.themoviedb.org/3/';
+  const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjFmN2E0ZmE4YjM4ODYyOTgzOTFiZWEwOTNjZGY1YSIsInN1YiI6IjVlODY0YjE4Mzk3NTYxMDAxMjk3YTNkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rXweNiZqdSsRIW5XFdZdgGhTMGGlamz39DJR9HVEwFY';
+
+  // const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const data = await response.json();
+
+  return data;
+}
+
 // Highlighte active link
 function highlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
@@ -20,6 +38,7 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case 'index.html':
+      displayPopularMovies();
       console.log('Home');
       break;
 
