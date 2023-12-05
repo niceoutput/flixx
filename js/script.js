968b1edf10d19693a76312598731ceea3d3ee978
mario <mario@uniplaces.com>
2023-12-05 00:03:58 +0000

    add fetching data dir popular movies section

--- js/script.js
+++ js/script.js
@@ -5,17 +5,47 @@ const global = {
 console.log(global.currentPage)
 
 async function displayPopularMovies() {
-  const results = await fetchAPIData('movie/popular');
-  console.log(results);
+  const { results } = await fetchAPIData('movie/popular');
+  results.forEach(({id, poster_path, title, release_date}) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${id}">
+      ${poster_path
+        ? `<img
+            src="https://image.tmdb.org/t/p/w500${poster_path}"
+            class="card-img-top"
+            alt="${title}"
+          />`
+        : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${title}"
+            />`
+      }
+
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${release_date}</small>
+        </p>
+      </div>
+    `;
+
+    const movieGrid = document.getElementById('popular-movies');
+
+    movieGrid.append(div);
+
+  })
 }
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = '5b1f7a4fa8b3886298391bea093cdf5a';
   const API_URL = 'https://api.themoviedb.org/3/';
-  const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YjFmN2E0ZmE4YjM4ODYyOTgzOTFiZWEwOTNjZGY1YSIsInN1YiI6IjVlODY0YjE4Mzk3NTYxMDAxMjk3YTNkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rXweNiZqdSsRIW5XFdZdgGhTMGGlamz39DJR9HVEwFY';
 
-  // const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
   const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
   const data = await response.json();
 
