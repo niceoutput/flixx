a43810036d002734aa1896f6c7227e663fcaf461
mario <mario@uniplaces.com>
2024-01-15 23:00:41 +0000

    add pagination

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
     term: '',
     type: '',
     page: 1,
-    totalPages: 1
+    totalPages: 1,
+    totalresults: 0
   },
   api: {
     apiKey: '5b1f7a4fa8b3886298391bea093cdf5a',
@@ -24,7 +25,11 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page} = await searchAPIData();
+    const { results, total_pages, page, total_results} = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert('No results found');
@@ -41,6 +46,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -69,11 +79,63 @@ function displaySearchResults(results) {
       </div>
     `;
 
+    document.querySelector('#search-results-heading').innerHTML = `
+      <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
     const movieGrid = document.getElementById('search-results');
 
     movieGrid.append(div);
 
   })
+
+  displayPagination();
+}
+
+// create and display Pagination
+function displayPagination() {
+  const div = document.createElement('div');
+
+  div.classList.add('pagination');
+
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  const previousButton = document.querySelector('#prev');
+  const nextButton = document.querySelector('#next');
+
+  // Disable Previous button if on first page
+  if (global.search.page === 1) {
+    previousButton.disabled = true;
+  }
+
+  // Disable Next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    nextButton.disabled = true;
+  }
+
+  // Next Page
+  nextButton.addEventListener("click", async () => {
+    global.search.page++;
+
+    const {results, total_pages} = await searchAPIData();
+
+    displaySearchResults(results);
+  });
+
+  // Previous page
+  previousButton.addEventListener("click", async () => {
+    global.search.page--;
+
+    const {results, total_pages} = await searchAPIData();
+
+    displaySearchResults(results);
+  });
 }
 
 // Display Slider
@@ -380,7 +442,7 @@ async function searchAPIData() {
   const API_URL = global.api.apiUrl;
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await response.json();
 
